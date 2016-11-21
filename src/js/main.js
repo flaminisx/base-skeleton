666b02fe797a88fe69c82de062727f14bd5c9f13
kolialike <kolialike@gmail.com>
2016-11-21 14:07:05 +0200

    init

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+jQuery(function($){
+  var appendNumber = 4;
+    var prependNumber = 1;
+    var swiper = new Swiper('.swiper-container', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 4,
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 20,
+    });
+    
+});
